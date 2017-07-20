93cb88b0117a964ce0de5c57feb6d370f019b273
meMEGAMIND <meMEGAVOID@gmail.com>
2017-07-20 13:05:34 -0500

    Done!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,15 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import static com.londonappbrewery.magiceightball.R.string.mantra;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView assurance = (TextView) findViewById(R.id.text);
+
+        assurance.setText("Make Everyday decisions easier!");
+
+        final ImageView ballDisplay =
+                (ImageView) findViewById(R.id.image_eightBall);
+
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        Button askButton;
+
+        askButton = (Button) findViewById(R.id.askButton);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random number = new Random();
+                int result = number.nextInt(5);
+
+                ballDisplay.setImageResource(ballArray[result]);
+
+            }
+        });
+
+
     }
 }
